48c98bd7b3e4f84c8035b53e3e79c4631f831d0c
carlosfernandez <carlosfernandez@10.34.137.143>
2014-10-15 17:03:52 +0200

    Rama andres ejercicio 21

---
+++ AccesoDatos/src/ut01/act04/Ejercicio04Test.java
@@ -0,0 +1,14 @@
+package ut01.act04;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Ejercicio04Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
