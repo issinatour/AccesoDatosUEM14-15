331a793fc67ee0a6069a5c3668a32b27ae5dc971
Alejandro <fj@10.34.80.55>
2014-10-16 17:04:00 +0200

    commit 02

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -3,21 +3,43 @@ package ut01.act04;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Ejercicio04 {
-	
-	public String contarMin (String path){
-		File  archivo = null;
+	/*
+	 * Cuenta el número de letras minúsculas puras (sin acentuar ni diéresis).
+	 */
+	public String contarMin(String path) {
+		File archivo = null;
 		FileReader fr = null;
+		StringBuffer contenido = new StringBuffer();
+		int suma = 0;
+
 		try {
-			 archivo = new File(path);
-			 fr = new FileReader(archivo);
-			
-		} catch (FileNotFoundException e) {
-			
+			archivo = new File(path);
+
+			fr = new FileReader(archivo);
+
+			int caracter;
+
+			while ((caracter = fr.read()) != -1) {
+				contenido.append((char) caracter);
+
+			}
+
+			for (int i = 0; i < contenido.length(); i++) {
+				if (contenido.toString().charAt(i) >= 'a'
+						&& contenido.toString().charAt(i) >= 'z') {
+					suma = contenido.toString().charAt(i)  ;
+				}
+			}
+			fr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println((char)suma);
 		return null;
-		
+
 	}
 }
--- AccesoDatos/src/ut01/act04/Main.java
+++ AccesoDatos/src/ut01/act04/Main.java
@@ -3,7 +3,8 @@ package ut01.act04;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Ejercicio04 ej = new Ejercicio04();
+		ej.contarMin("/Users/fj/Desktop/g.rtf");
 
 	}
 
