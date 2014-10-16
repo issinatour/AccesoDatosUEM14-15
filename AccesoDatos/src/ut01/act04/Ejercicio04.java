3751fa7856bb722d66f9a994127779634d38a176
Carlos <Carlos@10.34.85.79>
2014-10-16 16:10:58 +0200

    hola

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -1,5 +1,8 @@
 package ut01.act04;
 
 public class Ejercicio04 {
-
+	//Obtener la frecuencia de los nombres de los días de la semana 
+	//(cuántas veces se repite “lunes”, etcétera) y mostrar el resultado en su orden natural.
+	
+	
 }
