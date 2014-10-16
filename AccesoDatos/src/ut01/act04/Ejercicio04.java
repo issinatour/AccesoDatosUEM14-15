c10b2c9cf517e55833f295d0d4ba17a9de1feeb2
Alejandro <fj@10.34.80.55>
2014-10-16 16:17:18 +0200

    commit inicial

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -1,5 +1,23 @@
 package ut01.act04;
 
-public class Ejercicio04 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
+public class Ejercicio04 {
+	
+	public String contarMin (String path){
+		File  archivo = null;
+		FileReader fr = null;
+		try {
+			 archivo = new File(path);
+			 fr = new FileReader(archivo);
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
 }
