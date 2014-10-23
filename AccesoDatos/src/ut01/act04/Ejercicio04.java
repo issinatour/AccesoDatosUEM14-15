71b934347ad061776f096dba4059aa1d9b719ba6
carlosfernandez <carlosfernandez@10.34.82.229>
2014-10-23 16:06:52 +0200

    terminado act 01

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -1,5 +1,50 @@
 package ut01.act04;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejercicio04 {
+
+	public final static int TAM_BUFFER = 1024;
+
+	/**
+	 * return all printable characters of a text file
+	 * 
+	 * @param path
+	 *            of the text file
+	 * @return number of printable characters
+	 */
+	public int countCharacters(String path) {
+
+		BufferedReader br = null;
+		String buffer;
+		int numCharacters = 0;
+		try {
+			// abrimos flujo de lectura
+			br = new BufferedReader(new FileReader(new File(path)), TAM_BUFFER);
+			while ((buffer = br.readLine()) != null) {
+				numCharacters += buffer.length();
+
+			}
+		} catch (FileNotFoundException e) {
+
+		} catch (IOException e) {
+
+		} finally {
+
+			if (br != null)
+				try {
+					br.close();
+				} catch (IOException e) {
+				}
+
+		}
+
+		return numCharacters;
+
+	}
 
 }
