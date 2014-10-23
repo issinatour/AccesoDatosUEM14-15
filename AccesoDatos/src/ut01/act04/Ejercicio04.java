27ccbfabebdc3e09bb0bd565d11bd2a858b587aa
DAN1ELRL <danielrl.drl@gmail.com>
2014-10-23 18:00:38 +0200

    Ejercicio 11 terminado

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -7,51 +7,94 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Ejercicio04 {
-	public final int TAM_BUFFER=1024;
-	
-	public boolean isLetter(char letter){
+	public final int TAM_BUFFER = 1024;
+
+	public boolean isLetter(char letter) {
 		return Character.isLetter(letter);
 	}
-	
+
+	public boolean isPentavocalica(String word) {
+
+		int numa = 0, nume = 0, numi = 0, numo = 0, numu = 0;
+
+		for (int x = 0; x < word.length(); x++) {
+			if (word.charAt(x) == 'a') {
+				numa++;
+			}
+			if (word.charAt(x) == 'e') {
+				nume++;
+			}
+			if (word.charAt(x) == 'i') {
+				numi++;
+			}
+			if (word.charAt(x) == 'o') {
+				numo++;
+			}
+			if (word.charAt(x) == 'u') {
+				numu++;
+			}
+		}
+		if (numa == 1 && nume == 1 && numi == 1 && numo == 1 && numu == 1) {
+			return true;
+		} else
+			return false;
+	}
+
 	/**
 	 * find the first word pentavolica and return the position in the sequence.
+	 * 
 	 * @param path
-	 * @throws IOException 
+	 * @throws IOException
 	 * 
 	 */
 
-	public void findPentavolica(String path){
+	public int findPentavocalica(String path) {
 		BufferedReader br = null;
 		String buffer;
-		int numPalabras;
+		int numWords = 0;
 		char character;
-		boolean readingWord=false;
+		boolean readingWord = false;
 		StringBuffer wordReaden = new StringBuffer();
 		try {
-			br=new BufferedReader(new FileReader(new File(path)),TAM_BUFFER);
-			while((buffer=br.readLine()) != null){
-				for(int i=0;i<buffer.length();i++){
+			br = new BufferedReader(new FileReader(new File(path)), TAM_BUFFER);
+			while ((buffer = br.readLine()) != null) {
+				for (int i = 0; i < buffer.length(); i++) {
 					character = buffer.charAt(i);
-					if(isLetter(character) && !readingWord){
+					if (isLetter(character) && !readingWord) {
 						wordReaden.append(character);
-						readingWord=true;
-					}if(isLetter(character)&&readingWord){
-						
+						readingWord = true;
+
+					}
+					if (isLetter(character) && readingWord) {
+						wordReaden.append(character);
+					} else if (!isLetter(character) && readingWord) {
+
+					} else {
+						numWords++;
+						if (isPentavocalica(wordReaden.toString().toLowerCase())) {
+							// pintamos en pantalla la palabra pentavocalica
+							System.out.println(wordReaden);
+							return numWords;
+						}
+						wordReaden = new StringBuffer();
+						readingWord = false;
 					}
 				}
 			}
 		} catch (FileNotFoundException e) {
-			
-		}catch(IOException ex){
-			
-		}
-		finally{
+
+			System.out.println("No se encuentra el fichero");
+
+		} catch (IOException ex) {
+
+		} finally {
 			try {
-				if(br!=null)
+				if (br != null)
 					br.close();
 			} catch (IOException e) {
-				
+
 			}
 		}
+		return 0;
 	}
 }
--- AccesoDatos/src/ut01/act04/Main.java
+++ AccesoDatos/src/ut01/act04/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Ejercicio04 e = new Ejercicio04();
+		
+		System.out.println(e.findPentavocalica("./resources/Quijote.txt"));
 	}
 
 }
