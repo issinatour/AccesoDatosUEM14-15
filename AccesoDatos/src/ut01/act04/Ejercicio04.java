409873c9423812f25262df648fbc168bdf38bea2
Patrichuan <Patrichuan@gmail.com>
2014-10-26 23:39:39 +0100

    Update Ejercicio04.java

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -1,73 +1,64 @@
-package ut01.act04;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Ejercicio04 {
 
-	/* Obtener y mostrar la palabra más larga, su número de letras y su posición 
-	 * (número de orden en la secuencia de palabras). Si hay más de una se toma la primera.
-	 * Nota: El número de orden o posición de la palabra en el texto es el lugar que ocupa,
-	 * una vez contadas todas.
-	 */
+	/* Obtener y mostrar la palabra más larga, su número de letras y su posición
+	* (número de orden en la secuencia de palabras). Si hay más de una se toma la primera.
+	* Nota: El número de orden o posición de la palabra en el texto es el lugar que ocupa,
+	* una vez contadas todas.
+	*/	
 	
-	String ruta = "./resources/Quijote.txt";
-	
-	public String getPalabraMasLarga (String ruta) {
-		
-		File fichero = new File(ruta);		
-		String LineaLeida = "";	
-		String PalabraMayorEncontrada = "";
-		String CandidataAMayor, Resultado = null;		
-		StringTokenizer TextoTokenizado;		
-		int NumPalabrasDeLinea; 
-		int TamañoMayorEncontrado = 0;
-		
-		
-		if(fichero.isFile()){
-			FileReader fr = null;
-			int caracter;
-			try{
-				fr = new FileReader(fichero);
-				while((caracter = fr.read())!=-1){
-					char CaracterLeido = (char) caracter;
-					// Si no es fin de linea sigo concatenando los caracteres en LineaLeida
-					if (CaracterLeido != '\n'){
-						LineaLeida += CaracterLeido;
-						// cuando doy con \n se que he llegado al fin de la linea
-					} else {
-						// asi que paso toda la linea a StringTokenizer para contar las palabras
-						TextoTokenizado = new StringTokenizer(LineaLeida);
-						// Las cuento
-						NumPalabrasDeLinea = TextoTokenizado.countTokens();
-						// Y por cada una de ellas la guardo en CandidataAMayor
-						for (int x=0;x<NumPalabrasDeLinea;x++){
-							CandidataAMayor = TextoTokenizado.nextToken();
-							// Y miro si su longitud es la mayor encontrada hasta el momento
-						  	if (CandidataAMayor.length() > TamañoMayorEncontrado){
-						  		// En caso de serlo la guardo como la mayor que hay
-						  		PalabraMayorEncontrada = CandidataAMayor;
-						  		// Y me quedo con la longitud que tiene
-						  		TamañoMayorEncontrado = CandidataAMayor.length();
-						  	}
+	public String getPalabraMasLarga (String ruta) throws FileNotFoundException, IOException {
+		String LineaLeida, Token, Resultado = "";
+		int LongitudDelTokenMasLargo = 0;
+		String TokenMasLargo = "";		
+		FileInputStream Fis = null;
+		BufferedReader Br = null;
+		try {
+			Fis = new FileInputStream(ruta);
+			Br = new BufferedReader(new InputStreamReader(Fis));
+			LineaLeida = Br.readLine();
+			int NumDeToken = 1, PosicionToken = 1;
+			// Mientras siga teniendo mas lineas
+			while (LineaLeida != null) {
+				// Parto la linea en tokens
+				StringTokenizer LineaTokenizada = new StringTokenizer(LineaLeida);
+				// Y recorro dichos tokens
+				while (LineaTokenizada.hasMoreTokens()) {
+					Token = LineaTokenizada.nextToken();
+					// En caso de ser el token mas largo encontrado hasta el momento
+					// Me quedo con su posicion, su longitud y con el token					
+					if (Token.length() > LongitudDelTokenMasLargo) {
+						// Si no acaba en , entonces es valido el token
+						// ya que suma 1 caracter mas de longitud a la palabra
+						// y la longitud no seria real
+						if ((Token.charAt(Token.length()-1)!=',')) {     
+							PosicionToken = NumDeToken;
+							LongitudDelTokenMasLargo = Token.length();
+							TokenMasLargo = Token;
 						}
-					} 
-				}				
-			}
-			catch (IOException e){
-				System.err.println("Error al leer el fichero");
-			}
-			finally{
-				if (fr!=null)
-					try {
-						fr.close();
-					} catch (IOException e) {
-						
-					}					
+					}
+					// Aqui voy sumando 1 al numero de token para saber en todo momento
+					// la posicion del que estoy mirando
+					NumDeToken++;
+					Resultado = " Palabra: "+ TokenMasLargo + 
+							    "  | Longitud: "+ LongitudDelTokenMasLargo + 
+							    "  | Posicion: "+ PosicionToken + ".";					
+				}
+				// Y ahora sigo con la siguiente linea
+				LineaLeida = Br.readLine();
 			}
 		}
+		finally {
+			try {
+				Br.close();
+				Fis.close();
+			} 
+			catch (IOException ex) {
+			}
+		}
+		System.out.println(Resultado);
 		return Resultado;
-	}
+	}	
 }
