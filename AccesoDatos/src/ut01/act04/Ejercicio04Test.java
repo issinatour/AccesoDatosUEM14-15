244e641022d424d284bf64c76ff64105e6d4c2a4
Patrichuan <Patrichuan@gmail.com>
2014-10-26 23:40:10 +0100

    Update Ejercicio04Test.java

--- AccesoDatos/src/ut01/act04/Ejercicio04Test.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04Test.java
@@ -1,14 +1,22 @@
-package ut01.act04;
-
-import static org.junit.Assert.*;
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.junit.Test;
+
 
 public class Ejercicio04Test {
 
+	Ejercicio04 Ejercicio04= new Ejercicio04();
+	String ruta = "./resources/Quijote.txt";
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getPalabraMasLargaTEST() {
+		try {
+			Ejercicio04.getPalabraMasLarga(ruta);
+		} catch (FileNotFoundException e) {			
+			e.printStackTrace();
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
 	}
 
 }
