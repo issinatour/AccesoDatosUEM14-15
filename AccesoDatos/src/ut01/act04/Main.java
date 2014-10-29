a0428d6d096b983492c7cb239f8f5a0b902e5e93
David <David@David-PC>
2014-10-29 16:28:13 +0100

    CODIGO FINAL MAIN Y 04

--- AccesoDatos/src/ut01/act04/Main.java
+++ AccesoDatos/src/ut01/act04/Main.java
@@ -1,10 +1,20 @@
 package ut01.act04;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
-
-	public static void main(String[] args) {
+	
+	private static String ruta = "resources/Quijote.txt";
+			
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-
+		Ejercicio04 obj = new Ejercicio04(ruta);
+		try {
+			System.out.println(obj.getWordsDIPT());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
