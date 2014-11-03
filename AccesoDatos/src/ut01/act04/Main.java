6b4377a203128053ae7bc8126b4a562c3481727f
Carlos Tessier <prof.carlos.tessier@gmail.com>
2014-11-03 11:16:09 +0100

    added act 20

--- AccesoDatos/src/ut01/act04/Main.java
+++ AccesoDatos/src/ut01/act04/Main.java
@@ -3,10 +3,13 @@ package ut01.act04;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
 		String pathIn = "./resources/Quijote.txt";
 		String pathOut = "./resources/PalabrasEmpiezanMayuscula.txt";
 		Ejercicio04 e = new Ejercicio04();
 		e.WordStartLowercaseFile(pathIn, pathOut,2);
+		*/
+		
 	}
 
 }
