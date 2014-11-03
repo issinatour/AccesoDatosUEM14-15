db28050218d9832f1c9b1c61abdc7412cb0199f6
Carlos Tessier <prof.carlos.tessier@gmail.com>
2014-11-03 10:40:19 +0100

    act 19 added

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -152,5 +152,52 @@ public class Ejercicio04 {
 		}
 
 	}
+	
+	/**
+	 * Elimina los acentos de una línea
+	 * @param line
+	 * @return
+	 */
+	private String replaceAllAccents(String line) {
+		return line.replaceAll("Á", "A").replaceAll("É", "E").replaceAll("Í", "I").replaceAll("Ó", "O").replaceAll("Ú", "U").replaceAll("Ü", "U");
+
+	}
+
+	/**
+	 * Guarda el fichero en mayúsuculas y sin acéntos
+	 * @param pathIn
+	 * @param pathOut
+	 */
+	public void lowercaseFiletoUppercaseFile(String pathIn, String pathOut) {
+		// TODO Auto-generated method stub
+		BufferedReader streamIn = null;
+		BufferedWriter streamOut = null;
+		String line;
+		try {
+			streamIn = new BufferedReader(new FileReader(pathIn));
+			streamOut = new BufferedWriter(new FileWriter(pathOut));
+
+			while ((line = streamIn.readLine()) != null) {
+				line = line.toUpperCase();
+				line = replaceAllAccents(line);
+				System.out.println(line);
+				streamOut.write(line);
+			}
+
+		} catch (IOException e) {
+			System.err.println("Error I/O");
+		} finally {
+			if (streamIn != null)
+				try {
+					streamIn.close();
+				} catch (IOException e) {
+				}
+			if (streamOut != null)
+				try {
+					streamOut.close();
+				} catch (IOException e) {
+				}
+		}
+	}
 
 }
