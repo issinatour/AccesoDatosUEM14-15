645d343bed425fd56ac4a27d8c3898de6dca0583
Carlos Tessier <prof.carlos.tessier@gmail.com>
2014-11-03 10:12:49 +0100

    merge act04

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -1,84 +1,81 @@
 package ut01.act04;
 
-	/* Crear dos ficheros “PALMAYUS.TXT” y “PALMINUS.TXT”, 
- * con todas las palabras que empiezan por letra mayúscula y minúscula, 
- * respectivamente (cada palabra en una línea).
+/* Crear dos ficheros â€œPALMAYUS.TXTâ€� y â€œPALMINUS.TXTâ€�, 
+ * con todas las palabras que empiezan por letra mayÃºscula y minÃºscula, 
+ * respectivamente (cada palabra en una lÃ­nea).
  * 
  * */
-	
-	import java.io.File;
-	import java.io.FileReader;
-	import java.io.BufferedReader;
-	import java.io.IOException;
-	import java.io.StringReader;
-	import java.io.PrintWriter;
-	import java.io.BufferedWriter;
-	import java.io.FileWriter;
-	 
-	public class Ejercicio04 {
-	 
-		//declaramos las variables
-		private String texto="\n";
-		private String txt="";
-		public final int TAM_BUFFER = 1024;
-		int numPentas = 0;
-	
-		//constructor con los dos metodos
-		
-	    public Ejercicio04() {
-	    	leerArchivo();
-	    	guardarArchivo();
-	    }
-	 
-	    //metodo que lee el archivo para realizar la seleccion mayusculas y minusculas
-	    private void leerArchivo()
-	   {
-	      File nombre = new File("Quijote.txt");
-	 
-	         try{
-	               BufferedReader entrada = new BufferedReader(new FileReader(nombre));
-	               StringBuffer bufer = new StringBuffer();
-	               while (( texto = entrada.readLine() ) != null) 
-	                  bufer.append(texto + "\n");
-	 
-	               
-	               //metodo que coje el caracter primero como mayuscula o minuscula, falta por implementar
-	               
-	               /*  if(Character.isUpperCase(letra)) 
-          letra = Character.toLowerCase(letra);
-     else 
-          letra = Character.toUpperCase(letra); 
 
-     System.out.println(letra); 
-}  */
-	               texto = bufer.toString();
-	               System.out.println(texto);
-	 
-	               texto = texto.toUpperCase();
-	               System.out.println("\n" +texto);
-	           }
-	           catch(IOException e)
-	           {
-	               System.out.println(e);
-	           }
-	   }
-	 
-	    //metodo para guardar la modificacion en el fichero
-	   private void guardarArchivo()
-	   {
-		   	try { 
-			    BufferedReader leer = new BufferedReader(
-		                             new StringReader(texto));
-			    PrintWriter escribir = new PrintWriter(new BufferedWriter(new FileWriter("PALMAYUS.txt")));
-			    while ((txt = leer.readLine()) != null) 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.StringTokenizer;
+
+public class Ejercicio04 {
+
+	// declaramos las variables
+	private String texto = "\n";
+	private String txt = "";
+	public final int TAM_BUFFER = 1024;
+	int numPentas = 0;
+
+	// constructor con los dos metodos
+
+	public Ejercicio04() {
+		leerArchivo();
+		guardarArchivo();
+	}
+
+	// metodo que lee el archivo para realizar la seleccion mayusculas y
+	// minusculas
+	private void leerArchivo() {
+		File nombre = new File("Quijote.txt");
+
+		try {
+			BufferedReader entrada = new BufferedReader(new FileReader(nombre));
+			StringBuffer bufer = new StringBuffer();
+			while ((texto = entrada.readLine()) != null)
+				bufer.append(texto + "\n");
+
+			// metodo que coje el caracter primero como mayuscula o minuscula,
+			// falta por implementar
+
+			/*
+			 * if(Character.isUpperCase(letra)) letra =
+			 * Character.toLowerCase(letra); else letra =
+			 * Character.toUpperCase(letra);
+			 * 
+			 * System.out.println(letra); }
+			 */
+			texto = bufer.toString();
+			System.out.println(texto);
+
+			texto = texto.toUpperCase();
+			System.out.println("\n" + texto);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	// metodo para guardar la modificacion en el fichero
+	private void guardarArchivo() {
+		try {
+			BufferedReader leer = new BufferedReader(new StringReader(texto));
+			PrintWriter escribir = new PrintWriter(new BufferedWriter(
+					new FileWriter("PALMAYUS.txt")));
+			while ((txt = leer.readLine()) != null)
 				escribir.println(txt);
-			    escribir.close();
-			} catch (Exception e) { 
-			    System.out.println("Final");
-			}
-	   }
-	   
-
+			escribir.close();
+		} catch (Exception e) {
+			System.out.println("Final");
+		}
+	}
 
 	public boolean isLetter(char letter) {
 		return Character.isLetter(letter);
@@ -117,7 +114,7 @@ package ut01.act04;
 	 * 
 	 */
 
-	public void findPentavocalica(String path)   {
+	public void findPentavocalica(String path) {
 
 		String sLinea;
 		String sPalabra;
@@ -125,8 +122,7 @@ package ut01.act04;
 		// Cargamos el buffer con el contenido del archivo
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader(
-					"./resources/Quijote.txt"));
+			br = new BufferedReader(new FileReader("./resources/Quijote.txt"));
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -140,13 +136,13 @@ package ut01.act04;
 				// bucle por todas las palabras
 				while (st.hasMoreTokens()) {
 					sPalabra = st.nextToken();
-					
-					if(isPentavocalica(sPalabra)){
+
+					if (isPentavocalica(sPalabra)) {
 						numPentas++;
-						System.out.println("palabra ventavocalica: "+ sPalabra +", numero: "+numPentas);
+						System.out.println("palabra ventavocalica: " + sPalabra
+								+ ", numero: " + numPentas);
 
 					}
-					
 
 				}
 			} while ((sLinea = br.readLine()) != null);
@@ -157,5 +153,4 @@ package ut01.act04;
 
 	}
 
-	 
-	}
+}
