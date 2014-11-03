320dc4be540d5e39512dbd4ed2a0fc774686462c
Carlos Tessier <prof.carlos.tessier@gmail.com>
2014-11-03 10:39:39 +0100

    Act 19 finish

--- AccesoDatos/src/ut01/act04/Ejercicio04.java
+++ AccesoDatos/src/ut01/act04/Ejercicio04.java
@@ -1,61 +1,60 @@
 package ut01.act04;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringReader;
 
 
-public class Act19 {
-	String cadena= "/n";
-	String txt="";
-	public static void main(String[] args) {
+public class Ejercicio04 {
+
+
+	/**
+	 * Elimina los acentos de una línea
+	 * @param line
+	 * @return
+	 */
+	private String replaceAllAccents(String line) {
+		return line.replaceAll("Á", "A").replaceAll("É", "E").replaceAll("Í", "I").replaceAll("Ó", "O").replaceAll("Ú", "U").replaceAll("Ü", "U");
+
+	}
+
+	/**
+	 * Guarda el fichero en mayúsuculas y sin acéntos
+	 * @param pathIn
+	 * @param pathOut
+	 */
+	public void lowercaseFiletoUppercaseFile(String pathIn, String pathOut) {
 		// TODO Auto-generated method stub
-		new Act19();
-	}
-		public Act19(){
-			leerArchivo();
-			guardarArchivo();
-		
-	}
-		
-		
-		private void leerArchivo() {
-			// TODO Auto-generated method stub
-			File archivo = new File("QUIMAYUS.txt");
-			try{
-	               BufferedReader entrada = new BufferedReader(new FileReader(cadena));
-	               StringBuffer bufer = new StringBuffer();
-	               while (( cadena = entrada.readLine() ) != null) 
-	                  bufer.append(cadena + "\n");
-	 
-	               cadena = bufer.toString();
-	               System.out.println(cadena);
-	 
-	               cadena = cadena.toUpperCase();
-	               System.out.println("\n" +cadena);
-	           }
-	           catch(IOException e)
-	           {
-	               System.out.println(e);
-	           }
-		}
-		private void guardarArchivo() {
-			// TODO Auto-generated method stub
-			try { 
-			    BufferedReader lectura = new BufferedReader(new StringReader(cadena));
-			    PrintWriter escribir = new PrintWriter(new BufferedWriter(new FileWriter("QUIMAYUS.txt")));
-			    while ((txt = lectura.readLine()) != null) 
-				escribir.println(txt);
-			    escribir.close();
-			} catch (Exception e) { 
-			    System.out.println("Final");
+		BufferedReader streamIn = null;
+		BufferedWriter streamOut = null;
+		String line;
+		try {
+			streamIn = new BufferedReader(new FileReader(pathIn));
+			streamOut = new BufferedWriter(new FileWriter(pathOut));
+
+			while ((line = streamIn.readLine()) != null) {
+				line = line.toUpperCase();
+				line = replaceAllAccents(line);
+				System.out.println(line);
+				streamOut.write(line);
 			}
-	   }
+
+		} catch (IOException e) {
+			System.err.println("Error I/O");
+		} finally {
+			if (streamIn != null)
+				try {
+					streamIn.close();
+				} catch (IOException e) {
+				}
+			if (streamOut != null)
+				try {
+					streamOut.close();
+				} catch (IOException e) {
+				}
+		}
+	}
+
 }
-
-
-
--- AccesoDatos/src/ut01/act04/Main.java
+++ AccesoDatos/src/ut01/act04/Main.java
@@ -3,8 +3,13 @@ package ut01.act04;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String pathIn = "./resources/Quijote.txt";
+		String pathOut = "./resources/QuijoteMayusculas.txt";
 
+		Ejercicio04 e = new Ejercicio04();
+		e.lowercaseFiletoUppercaseFile(pathIn, pathOut);
+		
 	}
 
 }
