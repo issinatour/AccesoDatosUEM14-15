2484b8f11b3cfa48c45fbaa5cde9153d811c4d32
carlosfernandez <carlosfernandez@192.168.0.29>
2014-11-07 12:54:46 +0100

    added threads and new methods to EmployeeManager

--- AccesoDatos/src/ut01/random/EmployeeManager.java
+++ AccesoDatos/src/ut01/random/EmployeeManager.java
@@ -11,112 +11,230 @@ import java.util.StringTokenizer;
 public class EmployeeManager {
 
 	final static int TAM_NAME = 10;
+	final static int TAM_RECORD = 4 + 20 + 4 + 8; // 36
+	final static int TAM_MAX_FILE = TAM_RECORD*1000;
 
-	public boolean readCSVfile(String csvFile, String randomFile) {
+	public void readCSVfile(final String csvFile, final String randomFile) {
 
-		BufferedReader streamIn = null;
+		Thread t = new Thread(new Runnable() {
+
+			public void run() {
+
+				BufferedReader streamIn = null;
+				RandomAccessFile streamOut = null;
+				String line;
+				StringTokenizer st;
+				StringBuffer sb;
+
+				try {
+					streamOut = new RandomAccessFile(randomFile, "rw");
+					streamIn = new BufferedReader(new FileReader(csvFile));
+					while ((line = streamIn.readLine()) != null) {
+						// Leo el fichero csv y saco los datos
+						st = new StringTokenizer(line, ";");
+						while (st.hasMoreTokens()) {
+
+							int id = Integer.parseInt(st.nextToken());
+							streamOut.writeInt(id);// escribo en random id
+
+							sb = new StringBuffer(st.nextToken());
+							sb.setLength(TAM_NAME); // fijo a tam max 10
+							String name = sb.toString();
+							streamOut.writeChars(name); // nom bre
+							int dep = Integer.parseInt(st.nextToken());
+							streamOut.writeInt(dep);// departamento
+							double salary = Double.parseDouble(st.nextToken());
+							streamOut.writeDouble(salary);// salario
+
+						}
+					}
+
+				} catch (FileNotFoundException e) {
+
+				} catch (IOException e) {
+
+				} finally {
+					if (streamIn != null)
+						try {
+							streamIn.close();
+
+						} catch (IOException e) {
+
+						}
+					if (streamOut != null)
+						try {
+							streamOut.close();
+
+						} catch (IOException e) {
+
+						}
+				}
+
+			}
+		});
+		t.start();
+
+	}
+	
+	public  void modifyEmployee  (final String path, Employee emp)
+			throws Exception {
+		
+
 		RandomAccessFile streamOut = null;
-		String line;
-		StringTokenizer st;
+		
 		StringBuilder sb;
 
 		try {
-			streamOut = new RandomAccessFile(randomFile, "rw");
-			streamIn = new BufferedReader(new FileReader(csvFile));
-			while ((line = streamIn.readLine()) != null) {
-				// Leo el fichero csv y saco los datos
-				st = new StringTokenizer(line, ";");
-				while (st.hasMoreTokens()) {
+			streamOut = new RandomAccessFile(path, "rw");
+			
+			// cojo la posición del registro
+			int position = (emp.getId() - 1) * TAM_RECORD;
+			
+			// me posicion si existe el registro
+			if (position < TAM_MAX_FILE)
+				streamOut.seek(position);
+			else
+				throw new Exception("Tamaño de fichero excededido");
+			
+			streamOut.writeInt(emp.getId()); // escribo el id
+			// me aseguro que voy a tener un array de char de 10 caracteres
+			sb = new StringBuilder(emp.getName());
+			sb.setLength(TAM_NAME);
+			streamOut.writeChars(sb.toString());
 
-					int id = Integer.parseInt(st.nextToken());
-					streamOut.writeInt(id);// id
-					String name;
-					sb = new StringBuilder(st.nextToken());
-					sb.setLength(TAM_NAME); // relleno los huecos
-					name = sb.toString();
-					streamOut.writeChars(name); // nom bre
-					int dep = Integer.parseInt(st.nextToken());
-					streamOut.writeInt(dep);// departamento
-					double salary = Double.parseDouble(st.nextToken());
-					streamOut.writeDouble(salary);// salario
+			streamOut.writeInt(emp.getDep());
+			streamOut.writeDouble(emp.getSalary());
+			
+		
+		} catch (FileNotFoundException e) {
+			System.err.println("Fichero no encontrado");
+		} catch (IOException e) {
+			System.err.println("Error E/S");
 
+		} finally {
+			if (streamOut != null)
+				try {
+					streamOut.close();
+				} catch (IOException e) {
+					System.err.println("Error E/S");
 				}
+		}
+
+
+	}
+
+	public Employee getEmployee(final String path, final int clave)
+			throws Exception {
+		Employee emp = new Employee();
+
+		RandomAccessFile streamIn = null;
+		int id, dep;
+		double salary;
+		String sName;
+		char letter;
+		char[] aCharName = new char[TAM_NAME];
+
+		try {
+			streamIn = new RandomAccessFile(path, "r");
+			int position = (clave - 1) * TAM_RECORD;
+			if (position < streamIn.length())
+				streamIn.seek(position);
+			else
+				throw new Exception("Clave incorrecta");
+			id = streamIn.readInt();
+			for (int i = 0; i < TAM_NAME; i++) {
+				letter = streamIn.readChar();
+				if (letter != 0)
+					aCharName[i] = letter;
+				else
+					aCharName[i] = ' ';
 			}
+				sName = new String(aCharName);
+				dep = streamIn.readInt();
+				salary = streamIn.readDouble();
+				emp = new Employee(id, sName, dep, salary);
+			
 
 		} catch (FileNotFoundException e) {
-				return false;
 		} catch (IOException e) {
-			return false;
 
 		} finally {
 			if (streamIn != null)
 				try {
 					streamIn.close();
-
 				} catch (IOException e) {
-					return false;
-
-				}
-			if (streamOut != null)
-				try {
-					streamOut.close();
-
-				} catch (IOException e) {
-					return false;
-
 				}
 		}
 
-		return true;
-
+		return emp;
 	}
 
-	public void printEmployee(String path) {
+	public void printEmployee(final String path) {
 
-		// declara el fichero de acceso aleatorio
-		RandomAccessFile streamIn = null;
+		Thread t = new Thread(new Runnable() {
 
-		try {
-			streamIn = new RandomAccessFile(new File(path), "r");
+			public void run() {
+				// declara el fichero de acceso aleatorio
+				RandomAccessFile streamIn = null;
 
-			// variables para leer los datos
-			int id, dep, posicion;
-			Double salario;
-			char nombre[] = new char[TAM_NAME], aux;
-
-			do { // recorremos los arrays
-				id = streamIn.readInt(); // leemos el id de empleado
-				for (int i = 0; i < TAM_NAME; i++) {
-					aux = streamIn.readChar();// recorremos uno a uno los
-												// caracteres del apellido
-					if ((int) aux != 0)
-						nombre[i] = aux;
-					else
-						nombre[i] = ' '; // los vamos guardando en el array
-					// System.out.println((int)aux);
-				}
-				String nombreS = new String(nombre); // convertimos a String el
-														// array
-				dep = streamIn.readInt();// obtenemos el dep
-				salario = streamIn.readDouble(); // obtenemos el salario
-
-				System.out.println("ID: " + id + ", Apellido: " + nombreS + ","
-						+ " Departamento: " + dep + ", Salario: " + salario);
-				// posicion = posicion + 36; // nos situamos en el siguiente
-				// registro del empleado
-				// Cada empleado ocupa 36 bytes (4+20+4+8)
-				// Si he recorrido todos los bytes salimos del while
-
-			} while (streamIn.getFilePointer() <= streamIn.length());
-		} catch (FileNotFoundException e) {
-		} catch (IOException e) {
-		} finally {
-			if (streamIn != null)
 				try {
-					streamIn.close();// cerramos el flujo
+					streamIn = new RandomAccessFile(new File(path), "r");
+
+					// variables para leer los datos
+					int id, dep;
+					Double salario;
+					char nombre[] = new char[TAM_NAME], aux;
+
+					do { // recorremos los arrays
+						id = streamIn.readInt(); // leemos el id de empleado
+						for (int i = 0; i < TAM_NAME; i++) {
+							aux = streamIn.readChar();// recorremos uno a uno
+														// los
+														// caracteres del
+														// apellido
+							if ((int) aux != 0)
+								nombre[i] = aux;
+							else
+								nombre[i] = ' '; // los vamos guardando en el
+													// array
+							// System.out.println((int)aux);
+						}
+						String nombreS = new String(nombre); // convertimos a
+																// String el
+																// array
+						dep = streamIn.readInt();// obtenemos el dep
+						salario = streamIn.readDouble(); // obtenemos el salario
+
+						if(id!=0)
+						System.out.println("ID: " + id + ", Apellido: "
+								+ nombreS + "," + " Departamento: " + dep
+								+ ", Salario: " + salario);
+						// posicion = posicion + 36; // nos situamos en el
+						// siguiente
+						// registro del empleado
+						// Cada empleado ocupa 36 bytes (4+20+4+8)
+						// Si he recorrido todos los bytes salimos del while
+
+					} while (streamIn.getFilePointer() <= streamIn.length()); // leo
+																				// hasta
+																				// el
+																				// final
+																				// del
+																				// fichero
+				} catch (FileNotFoundException e) {
 				} catch (IOException e) {
+				} finally {
+					if (streamIn != null)
+						try {
+							streamIn.close();// cerramos el flujo
+						} catch (IOException e) {
+						}
 				}
-		}
+
+			}
+		});
+		t.start();
+
 	}
 
 }
--- AccesoDatos/src/ut01/random/Main.java
+++ AccesoDatos/src/ut01/random/Main.java
@@ -11,7 +11,36 @@ public class Main {
 		
 		em.readCSVfile(csvFile, randomFile);
 		
+		Employee nuevoEmpleado = new Employee(100,"Carlos",10,800);
+		
+		try {
+			em.modifyEmployee(randomFile, nuevoEmpleado);
+		} catch (Exception e) {
+			
+		}
+		
 		em.printEmployee(randomFile);
+		
+		Employee modificacionEmpleado = new Employee(4,"Juan",20,5000);
+		
+		//System.out.println(modificacionEmpleado);
+		try {
+			em.modifyEmployee(randomFile, modificacionEmpleado);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		
+		
+		Employee emp = new Employee();
+		try {
+			emp = em.getEmployee(randomFile, 4);
+		} catch (Exception e1) {
+		}
+		System.out.println(emp);
+		
+		
+		
 	}
 
 }
